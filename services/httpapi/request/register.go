2c15e836d9dcb851ab588190db7fc83d68158e7e
Pieter Lelaona <pieter@rebelworks.co>
2020-04-14 15:46:25 +0700

    fix typo

--- services/httpapi/request/register.go
+++ services/httpapi/request/register.go
@@ -2,10 +2,10 @@ package request
 
 // RegisterRequest ...
 type RegisterRequest struct {
-	Fname       string `josn:"fname" validate:"required"`
-	Lname       string `josn:"lname" validate:"required"`
-	Email       string `josn:"email" validate:"required"`
-	Phone       string `josn:"phone" validate:"required"`
-	Password    string `josn:"password" validate:"required"`
-	RecheckPass string `josn:"recheckPassword" validate:"required"`
+	Fname       string `json:"fname" validate:"required"`
+	Lname       string `json:"lname" validate:"required"`
+	Email       string `json:"email" validate:"required"`
+	Phone       string `json:"phone" validate:"required"`
+	Password    string `json:"password" validate:"required"`
+	RecheckPass string `json:"recheckPassword" validate:"required"`
 }
